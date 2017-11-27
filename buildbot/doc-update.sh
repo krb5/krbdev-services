ba42086e6a5fb72bc95bad2b1ede2815b604c1d9
Greg Hudson <ghudson@mit.edu>
2017-11-26 21:41:41 -0500

    Add buildbot doc update script

---
+++ buildbot/doc-update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+kinit -k -t /var/lib/buildbot/krbsnap.keytab krbsnap@ATHENA.MIT.EDU
+aklog athena.mit.edu
+trunksnap=/afs/athena.mit.edu/astaff/project/kerberos/krb5-current
+buildmaster=/var/lib/buildbot/master
+(cd $trunksnap && tar zxof $buildmaster/rst_html.tgz)
+kdestroy
+unlog
