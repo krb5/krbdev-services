3602db25f710f314a5e49ed0abdeeae592fd4f13
Greg Hudson <ghudson@mit.edu>
2017-12-06 13:01:18 -0500

    Fix doc-update.sh destination

--- buildbot/doc-update.sh
+++ buildbot/doc-update.sh
@@ -2,7 +2,7 @@
 set -e
 kinit -k -t /var/lib/buildbot/krbsnap.keytab krbsnap@ATHENA.MIT.EDU
 aklog athena.mit.edu
-trunksnap=/afs/athena.mit.edu/astaff/project/kerberos/krb5-current
+trunksnap=/afs/athena.mit.edu/astaff/project/kerberos/krb5-current/krb5-current
 buildmaster=/var/lib/buildbot/master
 (cd $trunksnap && tar zxof $buildmaster/rst_html.tgz)
 kdestroy
