3860e398c8cca83367688b14ca56f3b5a9053676
Greg Hudson <ghudson@mit.edu>
2019-07-15 00:36:31 -0400

    Update for Request Tracker 4.4.4
    Update rt-cvsgate for RT 4 APIs.  Update krb5-daily.sh to dump the rt4
    database and clean the RT 4 session data; for now it will continue to
    run out of /var/rt2.
    
    Remove send-pr-gate; we stopped shipping a krb5-send-pr in 1.13 and
    haven't seen bug reports in its format in a while.  Remove
    rt-mailgate, as we can use the stock version.
    
    Add RT and Apache httpd configuration files to rt-scripts, both
    updated for RT 4.

---
+++ rt-scripts/RT_SiteConfig.pm
@@ -0,0 +1,36 @@
+# Any configuration directives you include here will override
+# RT's default configuration file, RT_Config.pm
+#
+# To include a directive here, just copy the equivalent statement
+# from RT_Config.pm and change the value. We've included a single
+# sample value below.
+#
+# This file is actually a perl module, so you can include valid
+# perl code, as well.
+#
+# The converse is also true, if this file isn't valid perl, you're
+# going to run into trouble. To check your SiteConfig file, use
+# this comamnd:
+#
+#   perl -c /path/to/your/etc/RT_SiteConfig.pm
+
+Set($rtname, 'krbdev.mit.edu');
+Set($Organization , 'mit.edu');
+Set($OwnerEmail , 'ghudson@mit.edu');
+Set($CorrespondAddress , 'rt@krbdev.mit.edu');
+Set($CommentAddress , 'rt-comment@krbdev.mit.edu');
+Set($UseFriendlyToLine, 1);
+Set($WebPath, '/rt');
+Set($WebDomain, 'krbdev.mit.edu');
+Set($WebPort, 443);
+Set(@ReferrerWhitelist, qw(krbdev.mit.edu:444));
+Set($WebRemoteUserAuth, 1);
+Set($WebSecureCookies, 1);
+Set( %FullTextSearch,
+    Enable     => 1,
+    Indexed    => 1,
+    Column     => 'ContentIndex',
+    Table      => 'AttachmentsIndex',
+);
+#Set(@Plugins,(qw(Extension::QuickDelete RT::FM)));
+1;
--- rt-scripts/krb5-daily.sh
+++ rt-scripts/krb5-daily.sh
@@ -5,7 +5,7 @@ aklog athena.mit.edu
 f=rt.dump.`date +%Y.%m.%d`
 a=/afs/athena.mit.edu/astaff/project/krbdev/krbdev.mit.edu/backups/dumps
 d=/var/rt2/dumps
-pg_dump -U postgres -c -f $d/$f rt2
+pg_dump -U postgres -c -f $d/$f rt4
 gzip -9 $d/$f
 cp -p --no-preserve=ownership $d/$f.gz $a
 find $a -name 'rt.dump.*.gz' -mtime +3 -exec rm {} \;
@@ -13,6 +13,7 @@ find $d -name 'rt.dump.*.gz' -mtime +30 -exec rm {} \;
 #gzip -c -9 /var/repository-tar/krb5.tar >/var/repository-tar/krb5.tar.gz.tmp \
 #  &&mv /var/repository-tar/krb5.tar.gz.tmp /var/repository-tar/krb5.tar.gz
 #/var/postgres/bin/vacuumdb -U postgres -d rt2 -z -q
-find /var/rt2/WebRT/sessiondata -type f -mtime +1 -print|xargs rm
+find /opt/rt4/var/session_data -type f -mtime +1 -print0 | xargs -0 rm
+find /opt/rt4/var/mason_data -type f -mtime +1 -print0 | xargs -0 rm
 kdestroy
 unlog
