2fa15125da3344f4d5adbb5cfdf19c898bc16056
Greg Hudson <ghudson@mit.edu>
2021-02-17 12:24:13 -0500

    Improve krb5-daily.sh robustness
    Clean up old local dump files before trying to copy into AFS, so that
    they don't pile up if the krbdev locker is over quota.  Avoid "rm:
    missing operand" errors from the final two lines by just using find's
    -delete option.

--- rt-scripts/krb5-daily.sh
+++ rt-scripts/krb5-daily.sh
@@ -7,13 +7,13 @@ a=/afs/athena.mit.edu/astaff/project/krbdev/krbdev.mit.edu/backups/dumps
 d=/var/rt2/dumps
 pg_dump -U postgres -c -f $d/$f rt4
 gzip -9 $d/$f
+find $d -name 'rt.dump.*.gz' -mtime +30 -delete
 cp -p --no-preserve=ownership $d/$f.gz $a
-find $a -name 'rt.dump.*.gz' -mtime +3 -exec rm {} \;
-find $d -name 'rt.dump.*.gz' -mtime +30 -exec rm {} \;
+find $a -name 'rt.dump.*.gz' -mtime +3 -delete
 #gzip -c -9 /var/repository-tar/krb5.tar >/var/repository-tar/krb5.tar.gz.tmp \
 #  &&mv /var/repository-tar/krb5.tar.gz.tmp /var/repository-tar/krb5.tar.gz
 #/var/postgres/bin/vacuumdb -U postgres -d rt2 -z -q
-find /opt/rt4/var/session_data -type f -mtime +1 -print0 | xargs -0 rm
-find /opt/rt4/var/mason_data -type f -mtime +1 -print0 | xargs -0 rm
+find /opt/rt4/var/session_data -type f -mtime +1 -delete
+find /opt/rt4/var/mason_data -type f -mtime +1 -delete
 kdestroy
 unlog
